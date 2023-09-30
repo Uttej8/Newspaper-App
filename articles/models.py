776bc30732b20b6c4b330b33adf57201a2ff4efb
Uttej8 <uttejnaidu3001@gmail.com>
2023-09-30 15:42:10 +0530

    comments

--- articles/models.py
+++ articles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 from django.contrib.auth import get_user_model
 
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 # Create your models here.
 
@@ -24,3 +24,15 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse_lazy('article_detail', args=[str(self.id)])
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    comment = models.CharField(max_length=200)
+    author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,)
+
+    def __str__(self):
+        return self.comment
+    
+    def get_absolute_url(self):
+        return reverse('article_list')
